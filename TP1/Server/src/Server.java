096151835973ffa9d8d653c8a15d5f4b71bc44ae
Jaafar <jaafar.kaoussarani@polymtl.ca>
2020-01-14 21:49:47 -0500

    JK: Server IP verification fini

--- TP1/Server/src/Server.java
+++ TP1/Server/src/Server.java
@@ -8,17 +8,29 @@ import java.util.Scanner;
 
 public class Server {
 	private static ServerSocket listener;
+	private static Scanner input = new Scanner(System.in);
+	
+	public static String inputAndValidateIP() {
+		boolean isValid = false;
+		String ip = "";
+		String prompt = "Provide IP Address : ";
+		while(!isValid) {
+			System.out.println(prompt);
+			ip = input.next().strip();
+			String regex = "\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}";
+			isValid = ip.matches(regex);
+			prompt = "IP Address format is incorrect!\nProvide IP Address : ";
+		}
+		return ip;
+	}
 	
 	public static void main(String[] args) throws Exception{
 		int clientNumber = 0;
-		
-		Scanner input = new Scanner(System.in);
-
+	
 		System.out.println("Provide port number (5000-5050) : ");
 		int serverPort = input.nextInt();
-		
-		System.out.println("Provide IP Address : ");
-		String serverAdress = input.next();
+
+		String serverAdress = inputAndValidateIP();
 		
 		//Verify
 		serverAdress = "127.0.0.1";

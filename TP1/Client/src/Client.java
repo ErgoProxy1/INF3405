256bbfcb86d2eb31c1890581e5a2cf005d8aa6d5
Jaafar <jaafar.kaoussarani@polymtl.ca>
2020-01-14 21:47:43 -0500

    JK: Finis verification client IP Adresse

--- TP1/Client/src/Client.java
+++ TP1/Client/src/Client.java
@@ -4,18 +4,29 @@ import java.util.Scanner;
 
 public class Client {
 	private static Socket socket;
+	private static Scanner input = new Scanner(System.in);
+	
+	public static String inputAndValidateIP() {
+		boolean isValid = false;
+		String ip = "";
+		String prompt = "Provide IP Address : ";
+		while(!isValid) {
+			System.out.println(prompt);
+			ip = input.next().strip();
+			String regex = "\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}";
+			isValid = ip.matches(regex);
+			prompt = "IP Address format is incorrect!\nProvide IP Address : ";
+		}
+		return ip;
+	}
 	
 	public static void main(String[] args) throws Exception {
 		
 		System.out.println("Provide port number (5000-5050) : ");
-		Scanner input = new Scanner(System.in);
 		int port = input.nextInt();
 		
-
-		System.out.println("Provide IP Address : ");
-		String server = input.next();
+		String server = inputAndValidateIP();
 		
-
 		System.out.println("Provide Username : ");
 		String username = input.next();
 		
